a0e5c8b057bb5b5331d565ccb5a6ced18477e66c
Unk331 <unk331@bcsmet.com>
2024-12-22 21:50:17 -0600

    reset code
    Signed-off-by: Unk331 <unk331@bcsmet.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,9 @@
-#include <iostream>
-#include "math/geometry.h"
+#include<iostream>
 
-int main() {
-    using namespace Geometry;
-
-    // Circle example
-    Point circleCenter(5, 5);
-    Circle circle(circleCenter, 3);
-    circle.display();
-    std::cout << "\nArea: " << circle.area();
-    std::cout << "\nPerimeter: " << circle.perimeter();
-    std::cout << "\nContains Point (7, 5): " << (circle.containsPoint(Point(7, 5)) ? "Yes" : "No") << "\n";
-
-    // Rectangle example
-    Point rectTopLeft(0, 10);
-    Rectangle rectangle(rectTopLeft, 5, 3);
-    rectangle.display();
-    std::cout << "\nArea: " << rectangle.area();
-    std::cout << "\nPerimeter: " << rectangle.perimeter();
-    std::cout << "\nContains Point (2, 8): " << (rectangle.containsPoint(Point(2, 8)) ? "Yes" : "No") << "\n";
-
-    // Triangle example
-    Triangle triangle(Point(0, 0), Point(5, 0), Point(0, 5));
-    triangle.display();
-    std::cout << "\nArea: " << triangle.area();
-    std::cout << "\nPerimeter: " << triangle.perimeter();
-    std::cout << "\nContains Point (2, 2): " << (triangle.containsPoint(Point(2, 2)) ? "Yes" : "No") << "\n";
-
+int main(){
+    int a=10;
+    int b=20;
+    int c=a+b;
+    std::cout<<"The sum of a and b is "<<c<<std::endl;
     return 0;
 }
