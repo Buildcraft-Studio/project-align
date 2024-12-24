ef8c6bd09dd173671f5d074a68b015d7b288431f
Unk331 <unk331@bcsmet.com>
2024-12-23 23:45:38 -0600

    added lua support
    Signed-off-by: Unk331 <unk331@bcsmet.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include "lua.hpp"
 
-int main(){
-    int a=10;
-    int b=20;
-    int c=a+b;
-    std::cout<<"The sum of a and b is "<<c<<std::endl;
+int main() {
+    lua_State* L = luaL_newstate(); // Create a new Lua state
+    luaL_openlibs(L);              // Load Lua standard libraries
+
+    // Execute a Lua script
+    if (luaL_dostring(L, "print('Hello from Lua!')")) {
+        std::cerr << "Error: " << lua_tostring(L, -1) << std::endl;
+        lua_pop(L, 1); // Remove error message from the stack
+    }
+
+    // Clean up and close the Lua state
+    lua_close(L);
     return 0;
 }
