2d5b56f010712d066e931687051f66bc8b107b97
Unk331 <unk331@bcsmet.com>
2024-12-26 18:56:26 -0600

    basic number genreater for ID system
    Signed-off-by: Unk331 <unk331@bcsmet.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include "geometry/geometry.h"
 
+#include "randomID/uniID.h"
+
 int main() {
     std::vector<point> polygon = {{0, 0}, {4, 0}, {4, 4}, {0, 4}};
     point testPoint(2, 2);
@@ -12,5 +14,14 @@ int main() {
         std::cout << "The point is outside the polygon.\n";
     }
 
+
+    uniID localUD;
+        localUD.setSeed(12345);
+
+    std::cout << "Random ID: " << localUD.getUniID() << std::endl;
+    std::cout << "Random ID: " << localUD.getUniID() << std::endl;
+    std::cout << "Seed: " << localUD.getSeed() << std::endl;
     return 0;
+
+
 }
