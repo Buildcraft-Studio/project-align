ad81d6f3e58a5316d9649362e3083b301cb4c5d5
Unk331 <unk331@bcsmet.com>
2025-11-06 11:45:52 -0500

    added game env

---
+++ src/gameLogic/2d environment/materialType.hpp
@@ -0,0 +1,11 @@
+#ifndef MATERIALTPYE_HPP
+#define MATERIALTPYE_HPP
+namespace gameLogic {
+    enum class MaterialType {
+        SOLID,
+        LIQUID,
+        GAS,
+        PLASMA
+    };
+}
+#endif // MATERIALTPYE_HPP
